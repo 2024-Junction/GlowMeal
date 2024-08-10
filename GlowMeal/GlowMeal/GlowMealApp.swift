ed59e38b24268277e16aa7dfe99a9d20a4d88185
이준후 <hprize04@gmail.com>
2024-08-10 13:44:25 +0900

    project setup

---
+++ GlowMeal/GlowMeal/GlowMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowMealApp.swift
+//  GlowMeal
+//
+//  Created by 이준후 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GlowMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
